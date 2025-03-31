5ac758b533ca359d83422c6ad2b53870bc805326
Bisan <bisan.elghoul@gmail.com>
2025-04-01 00:30:49 +0300

    feat: implement subtraction operation

--- calculator.js
+++ calculator.js
@@ -1,6 +1,9 @@
 function calc(operand1, op, operand2) {
-    if (op === "+") {
-        return operand1 + operand2;
+    switch (op) {
+        case "+":
+            return operand1 + operand2;
+        case "-":
+            return operand1 - operand2;
     }
 }
 module.exports = calc;
