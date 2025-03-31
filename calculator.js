e98d47dce03f4e4c4e5a830be04a3649a0fbc136
Bisan <bisan.elghoul@gmail.com>
2025-04-01 00:34:14 +0300

    feat: implement multiplication operation

--- calculator.js
+++ calculator.js
@@ -4,6 +4,8 @@ function calc(operand1, op, operand2) {
             return operand1 + operand2;
         case "-":
             return operand1 - operand2;
+        case "*":
+            return operand1 * operand2;
     }
 }
 module.exports = calc;
