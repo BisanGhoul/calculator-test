8d5202a1e2e083f36c8ff93ba3ae8f38d53ea0f1
Bisan <bisan.elghoul@gmail.com>
2025-04-01 00:30:10 +0300

    test: subtratcion operation

--- calculator.test.js
+++ calculator.test.js
@@ -11,4 +11,15 @@ describe("Calculator", () => {
 
         expect(result).toBe(5);
     });
+
+    // Test case: Subtraction
+    it("should return the correct difference of two numbers", () => {
+        const operand1 = 5,
+            op = "-",
+            operand2 = 2;
+
+        const result = calc(operand1, op, operand2);
+
+        expect(result).toBe(3);
+    });
 });
