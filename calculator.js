31ca9882bb76e6b009afc1728a86e0e9c82c527d
Bisan <bisan.elghoul@gmail.com>
2025-04-01 12:41:26 +0300

    feat: handle division by zero

--- calculator.js
+++ calculator.js
@@ -7,6 +7,9 @@ function calc(operand1, op, operand2) {
         case "*":
             return operand1 * operand2;
         case "/":
+            if (operand2 === 0) {
+                throw new Error("Division by zero");
+            }
             return operand1 / operand2;
     }
 }
