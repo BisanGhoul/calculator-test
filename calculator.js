f4e388dbe74ffd99bf457e4bfde8ca57e151f228
Bisan <bisan.elghoul@gmail.com>
2025-04-01 13:12:41 +0300

    feat: throw error if the args provided are less than three

--- calculator.js
+++ calculator.js
@@ -1,4 +1,12 @@
-function calc(operand1, op, operand2) {
+function calc(...args) {
+    if (args.length < 3) {
+        throw new Error("At least one operator and two numbers are required");
+    }
+
+    const operand1 = args[0],
+        op = args[1],
+        operand2 = args[2];
+
     switch (op) {
         case "+":
             return operand1 + operand2;
